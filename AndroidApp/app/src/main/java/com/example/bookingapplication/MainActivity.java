10c996ad3975f3bd97b558d76aa2cfbf50020934
unknown <djordjevic.sv1.2021@uns.ac.rs>
2023-11-13 12:20:33 +0100

    Add SplashScreen

--- AndroidApp/app/src/main/java/com/example/bookingapplication/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/bookingapplication/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.bookingapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
 
+import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+
         super.onCreate(savedInstanceState);
+        splashScreen.setKeepOnScreenCondition(() -> true );
         setContentView(R.layout.activity_main);
+        
+        int SPLASH_TIME_OUT = 800;
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
