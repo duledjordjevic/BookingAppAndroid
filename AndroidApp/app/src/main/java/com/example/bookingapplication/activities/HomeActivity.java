9fb107977f62dc3f1475793136ece3ebac8d2df3
Maric002 Nikola Maric <n.maric1912@gmail.com>
2023-11-18 20:21:10 +0100

    Add text color for action bar and bottom navigation bar

--- AndroidApp/app/src/main/java/com/example/bookingapplication/activities/HomeActivity.java
+++ AndroidApp/app/src/main/java/com/example/bookingapplication/activities/HomeActivity.java
@@ -3,11 +3,13 @@ package com.example.bookingapplication.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.example.bookingapplication.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
@@ -23,6 +25,7 @@ public class HomeActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private NavController navController;
     private BottomNavigationView navView;
+    private AppBarConfiguration appBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,18 +35,24 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         toolbar = binding.toolbar;
+
         setSupportActionBar(toolbar);
+        getSupportActionBar();
 
         navView = binding.navView;
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+
+        appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_account, R.id.navigation_notifications,R.id.reservationsFragment,
-                R.id.addedPropertiesFragment,R.id.hostPropertiesFragment,R.id.commentsFragment,R.id.reportedUsersFragment)
+                R.id.addedPropertiesFragment,R.id.hostPropertiesFragment,R.id.commentsFragment,R.id.reportedUsersFragment,
+                R.id.loginActivity)
                 .build();
         navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_home);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        NavigationUI.setupWithNavController(navView, navController);
+
+
 
         Intent intent = getIntent();
         String uloga = intent.getStringExtra("Uloga");
@@ -64,6 +73,22 @@ public class HomeActivity extends AppCompatActivity {
                 break;
         }
     }
-    
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+        return true;
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
 
 }
