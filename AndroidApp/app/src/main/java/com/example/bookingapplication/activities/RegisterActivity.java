277d03f7b45f91a28dc9d118d04fa6518124aa73
unknown <djordjevic.sv1.2021@uns.ac.rs>
2023-11-18 15:28:51 +0100

    Add onClickListener in RegisterActivity to open LoginActivity

--- AndroidApp/app/src/main/java/com/example/bookingapplication/activities/RegisterActivity.java
+++ AndroidApp/app/src/main/java/com/example/bookingapplication/activities/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.example.bookingapplication.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -60,7 +61,8 @@ public class RegisterActivity extends AppCompatActivity {
         logInTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
             }
         });
     }
