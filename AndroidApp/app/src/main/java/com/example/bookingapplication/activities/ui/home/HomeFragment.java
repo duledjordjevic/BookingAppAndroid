57b23b6f4e2a2cd87bb00b4a2d11660e34196682
pejinovics <75089955+pejinovics@users.noreply.github.com>
2023-11-20 10:54:21 +0100

    Added implementation of home fragment usage

--- AndroidApp/app/src/main/java/com/example/bookingapplication/activities/ui/home/HomeFragment.java
+++ AndroidApp/app/src/main/java/com/example/bookingapplication/activities/ui/home/HomeFragment.java
@@ -1,18 +1,29 @@
 package com.example.bookingapplication.activities.ui.home;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.bookingapplication.R;
+import com.example.bookingapplication.activities.ui.datePicker.DatePickerFragment;
 import com.example.bookingapplication.databinding.FragmentHomeBinding;
 
-public class HomeFragment extends Fragment {
+import java.util.Arrays;
+import java.util.List;
+
+public class HomeFragment extends Fragment implements DatePickerDialog.OnDateSetListener{
 
     private FragmentHomeBinding binding;
 
@@ -24,6 +35,16 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        Spinner spinner = binding.btnSort;
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getActivity(),
+                android.R.layout.simple_spinner_item,
+                getResources().getStringArray(R.array.sort_arrays));
+        // Specify the layout to use when the list of choices appears
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(arrayAdapter);
+
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
@@ -33,5 +54,10 @@ public class HomeFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
     }
 }
