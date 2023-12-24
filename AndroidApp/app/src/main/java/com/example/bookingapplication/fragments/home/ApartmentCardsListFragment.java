c3fe1a5476d72dd343eaa38dd5d70d5097c09c15
pejinovics <pejinovicstefan@gmail.com>
2023-12-24 18:40:18 +0100

    feat: add apartment car list fragment

--- AndroidApp/app/src/main/java/com/example/bookingapplication/fragments/home/ApartmentCardsListFragment.java
+++ AndroidApp/app/src/main/java/com/example/bookingapplication/fragments/home/ApartmentCardsListFragment.java
@@ -2,65 +2,74 @@ package com.example.bookingapplication.fragments.home;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.example.bookingapplication.R;
+import com.example.bookingapplication.adapters.ApartmentCardsListAdapter;
+import com.example.bookingapplication.databinding.FragmentApartmentCardsListBinding;
+import com.example.bookingapplication.model.ApartmentCard;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link ApartmentCardsListFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ApartmentCardsListFragment extends Fragment {
+public class ApartmentCardsListFragment extends ListFragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private ApartmentCardsListAdapter adapter;
+    private static final String ARG_PARAM = "param";
+    private ArrayList<ApartmentCard> mProducts;
+    private FragmentApartmentCardsListBinding binding;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ApartmentCardsListFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ApartmentCardsListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ApartmentCardsListFragment newInstance(String param1, String param2) {
+    public static ApartmentCardsListFragment newInstance(ArrayList<ApartmentCard> products){
         ApartmentCardsListFragment fragment = new ApartmentCardsListFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putParcelableArrayList(ARG_PARAM, products);
         fragment.setArguments(args);
         return fragment;
     }
 
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.i("ShopApp", "onCreateView Products List Fragment");
+        binding = FragmentApartmentCardsListBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        return root;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i("ShopApp", "onCreate Products List Fragment");
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            mProducts = getArguments().getParcelableArrayList(ARG_PARAM);
+            adapter = new ApartmentCardsListAdapter(getActivity(), mProducts);
+            setListAdapter(adapter);
         }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_apartment_cards_list, container, false);
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
+
+    @Override
+    public void onListItemClick(@NonNull ListView l, @NonNull View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+    }
+
 }
