c70e01639ced482e434f1fc193c9a66cc97f15ad
Dusan Djordjevic <djordjevic.sv1.2021@uns.ac.rs>
2023-12-24 16:51:02 +0100

    Add jwt to all requests

--- AndroidApp/app/src/main/java/com/example/bookingapplication/util/SharedPreferencesManager.java
+++ AndroidApp/app/src/main/java/com/example/bookingapplication/util/SharedPreferencesManager.java
@@ -2,6 +2,7 @@ package com.example.bookingapplication.util;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.Log;
 
 import com.example.bookingapplication.model.User;
 import com.example.bookingapplication.model.enums.UserType;
@@ -12,16 +13,18 @@ public class SharedPreferencesManager {
     private static final String KEY_EMAIL = "email";
     private static final String KEY_USER_ROLE = "user_role";
     private static final String KEY_ID = "user_id";
+    private static final String KEY_JWT = "jwt";
 
     private static SharedPreferences getSharedPreferences(Context context) {
         return context.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE);
     }
 
-    public static void saveUserInfo(Context context, String email, UserType userType, int id) {
+    public static void saveUserInfo(Context context, String email, UserType userType, int id, String jwt) {
         SharedPreferences.Editor editor = getSharedPreferences(context).edit();
         editor.putString(KEY_EMAIL, email);
         editor.putString(KEY_USER_ROLE, userType.toString());
         editor.putInt(KEY_ID, id);
+        editor.putString(KEY_JWT, jwt);
         editor.apply();
     }
 
@@ -30,7 +33,9 @@ public class SharedPreferencesManager {
         String email = preferences.getString(KEY_EMAIL, "");
         String userRole = preferences.getString(KEY_USER_ROLE, UserType.ADMIN.toString());
         int id = preferences.getInt(KEY_ID, 0);
-        return new User((long) id, email, "",  UserType.valueOf(userRole), "");
+        String jwt = preferences.getString(KEY_JWT, "");
+        Log.d("REZ1", jwt);
+        return new User((long) id, email, "",  UserType.valueOf(userRole), jwt);
     }
 
     public static void clearUserInfo(Context context) {
@@ -38,6 +43,7 @@ public class SharedPreferencesManager {
         editor.remove(KEY_EMAIL);
         editor.remove(KEY_USER_ROLE);
         editor.remove(KEY_ID);
+        editor.remove(KEY_JWT);
         editor.apply();
     }
 }
