6adfb206257638d339b41e57b44dc963f19fe5bb
Dusan Djordjevic <djordjevic.sv1.2021@uns.ac.rs>
2024-01-14 17:18:23 +0100

    Add cancel and delete reservations functionality for guest

--- AndroidApp/app/src/main/java/com/example/bookingapplication/clients/ReservationService.java
+++ AndroidApp/app/src/main/java/com/example/bookingapplication/clients/ReservationService.java
@@ -8,8 +8,10 @@ import java.util.List;
 import java.util.Map;
 
 import retrofit2.Call;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 import retrofit2.http.QueryMap;
 
@@ -28,4 +30,20 @@ public interface ReservationService {
     })
     @GET("reservations/filterHost")
     Call<Collection<Reservation>> getFilteredReservationsForHost(@QueryMap Map<String, String> queryParams);
+
+    @Headers({
+            "User-Agent: Mobile-Android",
+            "Content-Type:application/json"
+    })
+    @DELETE("reservations/pending/{reservationId}")
+    Call<Boolean> deletePendingReservation(@Path("reservationId") Long reservationId);
+
+    @Headers({
+            "User-Agent: Mobile-Android",
+            "Content-Type:application/json"
+    })
+    @PUT("reservations/cancelAccepted/{reservationId}")
+    Call<Reservation> cancelAcceptedReservation(@Path("reservationId") Long reservationId);
+
+
 }
